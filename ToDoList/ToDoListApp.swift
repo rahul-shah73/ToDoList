954c64e9e26b66e6b4bd174cf3df3c5e2c88152f
DakuGabbar73 <rahulshah887859@gmail.com>
2025-05-24 10:41:35 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Rahul Shah on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
